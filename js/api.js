1177bc0b588635c7fda7996309caea51d433e86a
Flavio Costa <flavionegocios2004@gmail.com>
2026-01-30 19:46:44 -0300

    Implementando lógica para requisição 'PUT'.

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ const api = {
     try {
       const response = await fetch("http://localhost:3000/pensamentos");
       return await response.json();
-    } catch {
+    } catch (error) {
       alert("Erro ao buscar pensamentos");
       throw error;
     }
@@ -20,7 +20,34 @@ const api = {
       });
       return await response.json();
     } catch (error) {
+      alert("Erro ao salvar pensamentos");
+      throw error;
+    }
+  },
+  async buscarPensamentosPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+      return await response.json();
+    } catch (error) {
       alert("Erro ao buscar pensamentos");
+      throw error;
+    }
+  },
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/pensamentos/${pensamento.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(pensamento),
+        },
+      );
+      return await response.json();
+    } catch (error) {
+      alert("Erro ao editar pensamento!");
       throw error;
     }
   },
